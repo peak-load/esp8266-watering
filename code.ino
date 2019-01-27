3ff114ed58e7fabad670748622fc2d3245d8cc2a
peak-load <34206325+peak-load@users.noreply.github.com>
2019-01-27 23:16:41 +0100

    Update code.ino

--- code.ino
+++ code.ino
@@ -90,7 +90,7 @@ void reconnect() {
  while (!client.connected()) {
  Serial.print("Attempting MQTT connection...");
  // Attempt to connect
- if (client.connect("ESP8266 Client","peak-load", "password")) {
+ if (client.connect("ESP8266 Client", "peak-load", "password")) {
   Serial.println("");
   Serial.println("MQTT connected");
   Serial.println("");
