7b1fb2c94355d4dae355635d74b9d3d9779c66e3
peak-load <34206325+peak-load@users.noreply.github.com>
2019-01-27 23:16:26 +0100

    Update code.ino
    Added MQTT authentication for user 'peak-load' with 'passsword'

--- code.ino
+++ code.ino
@@ -90,7 +90,7 @@ void reconnect() {
  while (!client.connected()) {
  Serial.print("Attempting MQTT connection...");
  // Attempt to connect
- if (client.connect("ESP8266 Client")) {
+ if (client.connect("ESP8266 Client","peak-load", "password")) {
   Serial.println("");
   Serial.println("MQTT connected");
   Serial.println("");
